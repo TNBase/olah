61fc9a47b172c1a9c55bce348dd6e1dc22759a7f
Fergus <fergus.barratt00@gmail.com>
2025-02-18 08:19:36 +0000

    fix: change to uv

--- src/olah/database/models.py
+++ src/olah/database/models.py
@@ -6,7 +6,13 @@
 # https://opensource.org/licenses/MIT.
 
 import os
-from peewee import *
+from peewee import (
+    SqliteDatabase,
+    CharField,
+    Model,
+    DateTimeField,
+    BigIntegerField,
+)
 import datetime
 
 from olah.utils.olah_utils import get_olah_path
@@ -14,14 +20,17 @@ from olah.utils.olah_utils import get_olah_path
 db_path = os.path.join(get_olah_path(), "database.db")
 db = SqliteDatabase(db_path)
 
+
 class BaseModel(Model):
     class Meta:
         database = db
+
 
 class Token(BaseModel):
     token = CharField(unique=True)
     first_dt = DateTimeField()
     last_dt = DateTimeField()
+
 
 class DownloadLogs(BaseModel):
     id = CharField(unique=True)
@@ -33,15 +42,19 @@ class DownloadLogs(BaseModel):
     datetime = DateTimeField()
     token = CharField()
 
+
 class FileLevelLRU(BaseModel):
     org = CharField()
     repo = CharField()
     path = CharField()
     datetime = DateTimeField(default=datetime.datetime.now)
 
+
 db.connect()
-db.create_tables([
-    Token,
-    DownloadLogs,
-    FileLevelLRU,
-])
+db.create_tables(
+    [
+        Token,
+        DownloadLogs,
+        FileLevelLRU,
+    ]
+)
